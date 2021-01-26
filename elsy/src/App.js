3c63b7a65328a440b82b5e4980c8a6723f40121e
Nerfic <jdll.allan@gmail.com>
2021-01-26 17:31:08 +0100

    finish day02

--- elsy/src/App.js
+++ elsy/src/App.js
@@ -44,21 +44,47 @@ export class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      water: 0,
+      water: 1.5,
       heart: 120,
       temperature: -10,
       steps: 3000,
-      onHeartChange: (value) => { return this.setState({ heart: value.target.value }) },
-      onStepsChange: (value) => { return this.setState({ steps: value.target.value }) },
-      onTemperatureChange: (value) => { return this.setState({ temperature: value.target.value }) },
+      onHeartChange: (value) => {
+        this.setState({ heart: value.target.value });
+        this.calculateWater()
+      },
+      onStepsChange: (value) => {
+        this.setState({ steps: value.target.value });
+        this.calculateWater()
+      },
+      onTemperatureChange: (value) => {
+        this.setState({ temperature: value.target.value });
+        this.calculateWater()
+      },
     };
+  }
+
+  calculateWater = () => {
+
+    let result = 1.5;
+
+    if (this.state.temperature > 20) {
+      result += 0.02 * this.state.temperature;
+    }
+    if (this.state.heart > 120) {
+      result += 0.0008 * this.state.heart;
+    }
+    if (this.state.steps > 10000) {
+      result += 0.00002 * this.state.steps;
+    }
+    result = Math.round(result * 100) / 100;
+    this.setState({ water: result })
   }
 
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
-          <Box {...this.water} /> {/* Water */}
+          <Box {...this.water} value={this.state.water} /> {/* Water */}
           <Box {...this.steps} onChange={this.state.onStepsChange} value={this.state.steps} min={stepsMin} max={stepsMax} /> {/* Steps */}
           <Box {...this.heart} onChange={this.state.onHeartChange} value={this.state.heart} min={heartMin} max={heartMax} /> {/* Heart */}
           <Box {...this.temperature} onChange={this.state.onTemperatureChange} value={this.state.temperature} min={tempMin} max={tempMax} /> {/* Temperature */}
