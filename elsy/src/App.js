a50e065345e6ce928b665b38d5378e062d343704
Nerfic <jdll.allan@gmail.com>
2021-01-26 14:59:06 +0100

    finish input range

--- elsy/src/App.js
+++ elsy/src/App.js
@@ -8,11 +8,11 @@ const tempMax = 40;
 const heartMin = 80;
 const heartMax = 180;
 const stepsMin = 0;
-const septsMax = 50000;
+const stepsMax = 50000;
 
 export class App extends React.Component {
 
-  Water = {
+  water = {
     icon: "local_drink",
     color: "#3A85FF",
     value: 1.5,
@@ -20,21 +20,21 @@ export class App extends React.Component {
     type: "water"
   }
 
-  Steps = {
+  steps = {
     icon: "directions_walk",
     color: "black",
     value: 3000,
     unit: "steps"
   }
 
-  Heart = {
+  heart = {
     icon: "favorite",
     color: "red",
     value: 120,
-    unit: "bpm"
+    unit: "bpm",
   }
 
-  Temperature = {
+  temperature = {
     icon: "wb_sunny",
     color: "yellow",
     value: -10,
@@ -47,20 +47,21 @@ export class App extends React.Component {
       water: 0,
       heart: 120,
       temperature: -10,
-      steps: 3000
+      steps: 3000,
+      onHeartChange: (value) => { return this.setState({ heart: value.target.value }) },
+      onStepsChange: (value) => { return this.setState({ steps: value.target.value }) },
+      onTemperatureChange: (value) => { return this.setState({ temperature: value.target.value }) },
     };
   }
-  onHeartChange = function (value) {
-    this.setState({ heart: value })
-  }
+
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
-          <Box {...this.Water} /> {/* Water */}
-          <Box {...this.Steps} /> {/* Steps */}
-          <Box {...this.Heart} /> {/* Heart */}
-          <Box {...this.Temperature} /> {/* Temperature */}
+          <Box {...this.water} /> {/* Water */}
+          <Box {...this.steps} onChange={this.state.onStepsChange} value={this.state.steps} min={stepsMin} max={stepsMax} /> {/* Steps */}
+          <Box {...this.heart} onChange={this.state.onHeartChange} value={this.state.heart} min={heartMin} max={heartMax} /> {/* Heart */}
+          <Box {...this.temperature} onChange={this.state.onTemperatureChange} value={this.state.temperature} min={tempMin} max={tempMax} /> {/* Temperature */}
         </div>
       </div>
     );
--- elsy/src/components/Box.jsx
+++ elsy/src/components/Box.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class Steps extends React.Component {
+class Box extends React.Component {
     render() {
         return (
             <div className="box col-sm-3 col-6">
@@ -8,10 +8,10 @@ class Steps extends React.Component {
                     {this.props.icon}
                 </span>
                 <p>{this.props.value} {this.props.unit}</p>
-                {this.props.type !== "water" && <input type="range" min={this.props.min} max={this.props.max} value={this.props.value}></input>}
+                {this.props.type !== "water" && <input type="range" min={this.props.min} max={this.props.max} value={this.props.value} onChange={this.props.onChange}></input>}
             </div>
         )
     }
 }
 
-export default Steps;
+export default Box;
